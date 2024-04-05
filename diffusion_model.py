f748d7457898bb0aa7a7dc77faaa34c5e92f4ed4
kobikelemen <kobi.kelemen20@gmail.com>
2024-04-05 15:17:28 +0000

    cifar wip

--- diffusion_model.py
+++ diffusion_model.py
@@ -11,19 +11,21 @@ import matplotlib.pyplot as plt
 import tqdm
 from math import sqrt
 
-from unet import UNet
+# from unet import UNet
+from unet.unet import OneResUNet
 
-learning_rate = 1e-3
+learning_rate = 2e-4
 num_epochs = 10
-batch_size = 20
+batch_size = 128
 # beta = 0.01
-# min_beta = 10**-4
-# max_beta = 0.02
-min_beta = 0.01
-max_beta = 0.01
+min_beta = 10**-4
+max_beta = 0.02
+# min_beta = 0.01
+# max_beta = 0.01
 timesteps = 1000
-img_w = 28
-img_h = 28
+img_w = 32
+img_h = 32
+img_c = 3
 data_path = './data'
 device = 'cuda:0'
 
@@ -50,8 +52,8 @@ transform = transforms.Compose([
     transforms.Lambda(lambda x: (x - 0.5) * 2)
 ])
 
-train_dat = datasets.MNIST(data_path, train=True, download=True, transform=transform)
-test_dat = datasets.MNIST(data_path, train=False, transform=transform)
+train_dat = datasets.CIFAR10(data_path, train=True, download=True, transform=transform)
+test_dat = datasets.CIFAR10(data_path, train=False, transform=transform)
 
 loader_train = DataLoader(train_dat, batch_size, shuffle=True, generator=torch.Generator(device=device))
 loader_test = DataLoader(test_dat, batch_size, shuffle=False, generator=torch.Generator(device=device))
@@ -62,31 +64,24 @@ loader_test = DataLoader(test_dat, batch_size, shuffle=False, generator=torch.Ge
 
 def calc_xt(epsilon, t, x0):
     """ Note: this assumes beta doesn't vary with time step """
-    # at_hat = 1
-    # for s in range(1,t[0]+1):
-    #     at_hat *= (1 - calc_beta(min_beta, max_beta, s, timesteps))
     at_hat = at_hat_list[t]
-    # return torch.sqrt(at_hat) * x0 + torch.sqrt(1 - at_hat) * epsilon
-    return torch.sqrt(at_hat.view(batch_size,1,1,1)) * x0 + torch.sqrt(1 - at_hat.view(batch_size,1,1,1)) * epsilon
+    return torch.sqrt(at_hat.view(batch_size,1,1,1).repeat(1,img_c,1,1)) * x0 + torch.sqrt(1 - at_hat.view(batch_size,1,1,1).repeat(1,img_c,1,1)) * epsilon
 
 def sample_t(batch_size, timesteps):
-    # t = random.randint(1,timesteps)
-    # return torch.full((batch_size,), t)
     return torch.randint(1, timesteps, (batch_size,))
 
 def sample_epsilon(batch_size, img_h, img_w):
-    return torch.normal(torch.zeros((batch_size, 1, img_h, img_w)), torch.ones((batch_size, 1, img_h, img_w)))
+    return torch.normal(torch.zeros((batch_size, img_c, img_h, img_w)), torch.ones((batch_size, img_c, img_h, img_w)))
 
 def calc_loss(epsilon, epsilon_pred):
     return torch.linalg.vector_norm(epsilon - epsilon_pred)
 
 
 torch.set_default_device(device)
-model = UNet(num_steps=timesteps).to(device)
+model = OneResUNet(32, channels=img_c).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print("Total number of parameters is: {}".format(params))
-
 
 
 train_loss, test_loss = [], []
@@ -132,7 +127,7 @@ for epoch in range(num_epochs):
         
 
         if epoch_test_loss < min_test_loss:
-            torch.save(model.state_dict(), '/home/kk2720/dl/diffusion-model/model/mnist_simple_diffusion12.pt')
+            torch.save(model.state_dict(), '/home/kk2720/dl/diffusion-model/model/cifar_simple_diffusion1.pt')
             min_test_loss = epoch_test_loss
 
 
@@ -141,4 +136,4 @@ plt.plot(epochs, train_loss, label='Train Loss')
 plt.plot(epochs, test_loss, label='Test Loss')
 plt.title('Loss')
 plt.ylabel('Epochs')
-plt.savefig('/home/kk2720/dl/diffusion-model/plots/simple_diffusion_loss12.jpeg')
+plt.savefig('/home/kk2720/dl/diffusion-model/plots/cifar_diffusion_loss1.jpeg')
---
+++ unet/unet.py
@@ -0,0 +1,410 @@
+"""
+Original unet implementation:
+https://github.com/Camaltra/this-is-not-real-aerial-imagery/blob/main/src/ai/models/u_net.py 
+"""
+
+
+import torch
+import torch.nn as nn
+from .utils import default
+from .composants import Upsample, DownSample, ConvNextBlock, SinusoidalPosEmb, BlockAttention
+
+
+class OneResUNet(nn.Module):
+    def __init__(
+            self,
+            dim,
+            init_dim=None,
+            out_dim=None,
+            dim_mults=(1, 2, 4, 8),
+            channels=3,
+            sinusoidal_pos_emb_theta=10000,
+            convnext_block_groups=8,
+    ):
+        super().__init__()
+        self.channels = channels
+        input_channels = channels
+        self.init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv2d(input_channels, self.init_dim, 7, padding=3)
+
+        dims = [self.init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        sinu_pos_emb = SinusoidalPosEmb(dim, theta=sinusoidal_pos_emb_theta)
+
+        time_dim = dim * 4
+
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim),
+        )
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        ConvNextBlock(
+                            in_channels=dim_in,
+                            out_channels=dim_in,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        ConvNextBlock(
+                            in_channels=dim_in,
+                            out_channels=dim_in,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        DownSample(dim_in, dim_out)
+                        if not is_last
+                        else nn.Conv2d(dim_in, dim_out, 3, padding=1),
+                    ]
+                )
+            )
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ConvNextBlock(mid_dim, mid_dim, time_embedding_dim=time_dim)
+        self.mid_block2 = ConvNextBlock(mid_dim, mid_dim, time_embedding_dim=time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1)
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        ConvNextBlock(
+                            in_channels=dim_out + dim_in,
+                            out_channels=dim_out,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        ConvNextBlock(
+                            in_channels=dim_out,
+                            out_channels=dim_out,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        Upsample(dim_out, dim_in)
+                        if not is_last
+                        else nn.Conv2d(dim_out, dim_in, 3, padding=1),
+                    ]
+                )
+            )
+
+        default_out_dim = channels
+        self.out_dim = default(out_dim, default_out_dim)
+
+        self.final_res_block = ConvNextBlock(dim * 2, dim, time_embedding_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time):
+        b, _, h, w = x.shape
+        x = self.init_conv(x)
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        unet_stack = []
+        for down1, down2, downsample in self.downs:
+            x = down1(x, t)
+            x = down2(x, t)
+            unet_stack.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_block2(x, t)
+
+        for up1, up2, upsample in self.ups:
+            x = torch.cat((x, unet_stack.pop()), dim=1)
+            x = up1(x, t)
+            x = up2(x, t)
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+        x = self.final_res_block(x, t)
+
+        return self.final_conv(x)
+
+
+class TwoResUNet(nn.Module):
+    def __init__(
+        self,
+        dim,
+        init_dim=None,
+        out_dim=None,
+        dim_mults=(1, 2, 4, 8),
+        channels=3,
+        sinusoidal_pos_emb_theta=10000,
+        convnext_block_groups=8,
+    ):
+        super().__init__()
+        self.channels = channels
+        input_channels = channels
+        self.init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv2d(input_channels, self.init_dim, 7, padding=3)
+
+        dims = [self.init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        sinu_pos_emb = SinusoidalPosEmb(dim, theta=sinusoidal_pos_emb_theta)
+
+        time_dim = dim * 4
+
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim),
+        )
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        ConvNextBlock(
+                            in_channels=dim_in,
+                            out_channels=dim_in,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        ConvNextBlock(
+                            in_channels=dim_in,
+                            out_channels=dim_in,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        DownSample(dim_in, dim_out)
+                        if not is_last
+                        else nn.Conv2d(dim_in, dim_out, 3, padding=1),
+                    ]
+                )
+            )
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ConvNextBlock(mid_dim, mid_dim, time_embedding_dim=time_dim)
+        self.mid_block2 = ConvNextBlock(mid_dim, mid_dim, time_embedding_dim=time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1)
+            is_first = ind == 0
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        ConvNextBlock(
+                            in_channels=dim_out + dim_in,
+                            out_channels=dim_out,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        ConvNextBlock(
+                            in_channels=dim_out + dim_in,
+                            out_channels=dim_out,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        Upsample(dim_out, dim_in)
+                        if not is_last
+                        else nn.Conv2d(dim_out, dim_in, 3, padding=1)
+                    ]
+                )
+            )
+
+        default_out_dim = channels
+        self.out_dim = default(out_dim, default_out_dim)
+
+        self.final_res_block = ConvNextBlock(dim * 2, dim, time_embedding_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time):
+        b, _, h, w = x.shape
+        x = self.init_conv(x)
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        unet_stack = []
+        for down1, down2, downsample in self.downs:
+            x = down1(x, t)
+            unet_stack.append(x)
+            x = down2(x, t)
+            unet_stack.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_block2(x, t)
+
+        for up1, up2, upsample in self.ups:
+            x = torch.cat((x, unet_stack.pop()), dim=1)
+            x = up1(x, t)
+            x = torch.cat((x, unet_stack.pop()), dim=1)
+            x = up2(x, t)
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+        x = self.final_res_block(x, t)
+
+        return self.final_conv(x)
+
+
+class AttentionUNet(nn.Module):
+    def __init__(
+        self,
+        dim,
+        init_dim=None,
+        out_dim=None,
+        dim_mults=(1, 2, 4, 8),
+        channels=3,
+        sinusoidal_pos_emb_theta=10000,
+        convnext_block_groups=8,
+    ):
+        super().__init__()
+        self.channels = channels
+        input_channels = channels
+        self.init_dim = default(init_dim, dim)
+        self.init_conv = nn.Conv2d(input_channels, self.init_dim, 7, padding=3)
+
+        dims = [self.init_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        sinu_pos_emb = SinusoidalPosEmb(dim, theta=sinusoidal_pos_emb_theta)
+
+        time_dim = dim * 4
+
+        self.time_mlp = nn.Sequential(
+            sinu_pos_emb,
+            nn.Linear(dim, time_dim),
+            nn.GELU(),
+            nn.Linear(time_dim, time_dim),
+        )
+
+        self.downs = nn.ModuleList([])
+        self.ups = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.downs.append(
+                nn.ModuleList(
+                    [
+                        ConvNextBlock(
+                            in_channels=dim_in,
+                            out_channels=dim_in,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        ConvNextBlock(
+                            in_channels=dim_in,
+                            out_channels=dim_in,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        DownSample(dim_in, dim_out)
+                        if not is_last
+                        else nn.Conv2d(dim_in, dim_out, 3, padding=1),
+                    ]
+                )
+            )
+
+        mid_dim = dims[-1]
+        self.mid_block1 = ConvNextBlock(mid_dim, mid_dim, time_embedding_dim=time_dim)
+        self.mid_block2 = ConvNextBlock(mid_dim, mid_dim, time_embedding_dim=time_dim)
+
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out)):
+            is_last = ind == (len(in_out) - 1)
+            is_first = ind == 0
+
+            self.ups.append(
+                nn.ModuleList(
+                    [
+                        BlockAttention(dim_out, dim_in, 2 if not is_first else 1),
+                        ConvNextBlock(
+                            in_channels=dim_out + dim_in,
+                            out_channels=dim_out,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        ConvNextBlock(
+                            in_channels=dim_out + dim_in,
+                            out_channels=dim_out,
+                            time_embedding_dim=time_dim,
+                            group=convnext_block_groups,
+                        ),
+                        Upsample(dim_out, dim_in)
+                        if not is_last
+                        else nn.Conv2d(dim_out, dim_in, 3, padding=1)
+                    ]
+                )
+            )
+
+        default_out_dim = channels
+        self.out_dim = default(out_dim, default_out_dim)
+
+        self.final_res_block = ConvNextBlock(dim * 2, dim, time_embedding_dim=time_dim)
+        self.final_conv = nn.Conv2d(dim, self.out_dim, 1)
+
+    def forward(self, x, time):
+        b, _, h, w = x.shape
+        x = self.init_conv(x)
+        r = x.clone()
+
+        t = self.time_mlp(time)
+
+        unet_stack = []
+        for down1, down2, downsample in self.downs:
+            x = down1(x, t)
+            unet_stack.append(x)
+            x = down2(x, t)
+            unet_stack.append(x)
+            x = downsample(x)
+
+        x = self.mid_block1(x, t)
+        x = self.mid_block2(x, t)
+
+        for attention, up1, up2, upsample in self.ups:
+            attention_out = attention(unet_stack.pop(), x)
+            x = torch.cat((x, attention_out), dim=1)
+            x = up1(x, t)
+            attention_out = attention(unet_stack.pop(), x)
+            x = torch.cat((x, attention_out), dim=1)
+            x = up2(x, t)
+            x = upsample(x)
+
+        x = torch.cat((x, r), dim=1)
+        x = self.final_res_block(x, t)
+
+        return self.final_conv(x)
+
+
+if __name__ == "__main__":
+    img_in = torch.randn(1, 3, 32, 32).to("cuda:0")
+    timestamp_in = torch.randn(1).to("cuda:0")
+
+    models = [
+        OneResUNet(64, channels=3).to("cuda:0"),
+        # TwoResUNet(64, channels=3).to("mps"),
+        # AttentionUNet(64, channels=3).to("mps")
+    ]
+
+    for model in models:
+        # print(model)
+        params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print("Total number of parameters is: {}".format(params))
+        output = model(img_in, timestamp_in)
+        assert output.shape == img_in.shape, "Not the same shape as input"
+    print("Success!")
